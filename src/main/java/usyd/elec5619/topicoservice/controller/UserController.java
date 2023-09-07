baedb04da67c13e443dbcd2e7e7be783ee76c496
Taohan Kang <46840416+takosenpai2687@users.noreply.github.com>
2023-09-07 20:10:00 +1000

    Update UserController.java

--- src/main/java/usyd/elec5619/topicoservice/controller/UserController.java
+++ src/main/java/usyd/elec5619/topicoservice/controller/UserController.java
@@ -1,4 +1,10 @@
 package usyd.elec5619.topicoservice.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class UserController {
+
 }
