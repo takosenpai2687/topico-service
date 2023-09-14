a2bb011d8b4aa66224516ab5acd856c498f217ed
Taohan Kang <kangtaohan9863@gmail.com>
2023-09-14 23:04:44 +1000

    feat: comm posts

--- src/main/java/usyd/elec5619/topicoservice/controller/CommunityController.java
+++ src/main/java/usyd/elec5619/topicoservice/controller/CommunityController.java
@@ -3,15 +3,17 @@ package usyd.elec5619.topicoservice.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import usyd.elec5619.topicoservice.mapper.UserMapper;
 import usyd.elec5619.topicoservice.model.UserCommunity;
 import usyd.elec5619.topicoservice.pojo.CommonResponse;
 import usyd.elec5619.topicoservice.service.CheckinService;
 import usyd.elec5619.topicoservice.service.CommunityService;
+import usyd.elec5619.topicoservice.service.PostService;
 import usyd.elec5619.topicoservice.service.UserService;
+import usyd.elec5619.topicoservice.type.SortBy;
+import usyd.elec5619.topicoservice.vo.Pager;
+import usyd.elec5619.topicoservice.vo.PostVO;
 
 @RestController("/api/v1/communities")
 @RequiredArgsConstructor
@@ -20,6 +22,7 @@ public class CommunityController {
     private final UserService userService;
     private final CheckinService checkinService;
     private final CommunityService communityService;
+    private final PostService postService;
 
     @PostMapping("/checkin/{communityId}")
     public CommonResponse<UserCommunity> checkin(Authentication authentication, @Valid @PathVariable Long communityId) {
@@ -44,5 +47,11 @@ public class CommunityController {
         final Long userId = userService.emailToId(email);
         final UserCommunity userCommunity = communityService.unfollow(userId, communityId);
         return CommonResponse.success(userCommunity);
+    }
+
+    @GetMapping("/posts/{communityId}")
+    public CommonResponse<Pager<PostVO>> getCommunityPosts(@PathVariable Long communityId, @RequestParam(defaultValue = "1") Integer page, @RequestParam(defaultValue = "10") Integer size, @RequestParam(defaultValue = "MOST_LIKES") SortBy sortBy) {
+        final Pager<PostVO> posts = postService.getPostsByCommunityId(communityId, page, size, sortBy);
+        return CommonResponse.success(posts);
     }
 }
