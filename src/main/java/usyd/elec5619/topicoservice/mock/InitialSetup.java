f0f24ffe6ad99bd07ac01f5b1ba2a66ee9529781
takosenpai2687 <kangtaohan9863@gmail.com>
2023-10-29 01:40:40 +1100

    delete mock comm

--- src/main/java/usyd/elec5619/topicoservice/mock/InitialSetup.java
+++ src/main/java/usyd/elec5619/topicoservice/mock/InitialSetup.java
@@ -12,15 +12,12 @@ import usyd.elec5619.topicoservice.model.User;
 @RequiredArgsConstructor
 public class InitialSetup {
     private final MockAdmin mockAdmin;
-//    private final MockCommunity mockCommunity;
 
     @Bean
     CommandLineRunner initialDataSetup() {
         return (args) -> {
             User admin = mockAdmin.createAdmin();
             log.info("Admin created with id: {}, email: {}, password: {}", admin.getId(), admin.getEmail(), mockAdmin.adminPassword);
-//            mockCommunity.createCommunities();
-//            log.info("Communities created");
         };
     }
 
